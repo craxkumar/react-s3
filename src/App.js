32634a37802c85db85f67197fdea7e7f78f58f0e
Rakesh kumar <rakesh.c@comakeit.com>
2022-11-23 00:13:26 +0530

    changes to check CF

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Hi THis IS S3 DeMo APP BY RAKESH KUMAR.
+          Edit <code>src/App.js</code> and save to reload Hi THis IS S3 DeMo APP BY RAKESH KUMAR
+          commit to cloud front with chnages.
         </p>
         <a
           className="App-link"
